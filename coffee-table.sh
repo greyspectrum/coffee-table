873f2ac215d265c30ee36af395e91626c0fc7a1e
greyspectrum <greyspectrum@users.noreply.github.com>
2016-07-25 20:41:55 +0000

    Update coffee-table.sh

--- coffee-table.sh
+++ coffee-table.sh
@@ -5,7 +5,7 @@
 # -----------
 # Isn't it annoying when you can't get a seat at a coffee shop?
 #
-# Of course it is. Kill the wifi with aircrack ng and clear the place out.
+# Of course it is. Kill the wifi with aircrack-ng and clear the place out.
 #
 # Now, pull up a chair and enjoy :)
 #
