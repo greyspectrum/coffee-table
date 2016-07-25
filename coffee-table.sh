4287f1662cd0a2f52df273f00bb4966273175dcc
greyspectrum <greyspectrum@users.noreply.github.com>
2016-07-25 20:18:22 +0000

    Create coffee-table.sh

---
+++ coffee-table.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+##############################################################################
+# coffee-table
+# -----------
+# Isn't it annoying when you can't get a seat at a coffee shop?
+#
+# Of course it is. Kill the wifi with aircrack ng and clear the place out.
+#
+# Now, pull up a chair and enjoy :)
+#
+# Definitely illegal. Use at your own risk.
+# 
+# :author: greyspectrum
+# :date:  July 25 2016
+# :version: 0.0.1
+##############################################################################
